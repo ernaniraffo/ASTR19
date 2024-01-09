6ccc6ae46db6db56c2e57cd36e84751321a1b954
ernaniraffo <ernani.raffo@icloud.com>
2024-01-09 12:40:25 -0800

    hello world script

---
+++ helloworld.py
@@ -0,0 +1,16 @@
+# A program that prints out Hello World!
+
+
+# Prints Hello World
+def PrintHelloWorld():
+    print("Hello World!")
+
+
+# The main function of our program
+def main():
+    PrintHelloWorld()
+
+
+# Check if this program is the main program
+if __name__ == "__main__":
+    main()
