81519f7571f9ec1d6027e094b0c5fb684afc695e
ernaniraffo <ernani.raffo@icloud.com>
2024-01-11 12:15:55 -0800

    Added function and module docstrings

--- code-journal-1/session2.py
+++ code-journal-1/session2.py
@@ -1,42 +1,50 @@
-# Prompt:
-# Write a Python program that prints the sum of two floating point numbers, 
-# the difference between two integers, and the product of a floating point number and an integer. 
-# In each case, have the program print out the data type of the resulting answer.
+"""
+Prompt:
+Write a Python program that prints the sum of two floating point numbers, 
+the difference between two integers, and the product of a floating point number and an integer. 
+In each case, have the program print out the data type of the resulting answer.
+"""
 
 
 from random import random, randint
 
 
-# Print the result and its data type
 def print_result_and_type(f: callable, args: list[int | float], res: int | float) -> None:
+    """Print the result and its data type"""
+
     print(f"result of {f}({args[0]}, {args[1]}) = ({res.__class__}, {res})")
 
 
-# Make sure the class of the number matches the type expected
-def is_type(x: int | float, type: float | int) -> bool:
-    return x.__class__ == type
+def is_type(x: int | float, data_type: float | int) -> bool:
+    """Make sure the class of the number matches the type expected"""
+
+    return x.__class__ == data_type
 
 
-# Computes sum of two floating point numbers
 def fsum(x: float, y: float) -> float:
+    """Computes sum of two floating point numbers"""
+
     assert is_type(x, float) and is_type(y, float)
     return x + y
 
 
-# Computes the difference of two integers
 def idef(x: int, y: int) -> int:
+    """Computes the difference of two integers"""
+
     assert is_type(x, int) and is_type(y, int)
     return x + y
 
 
-# Computer the product of a floating point number and an integer
 def fmulti(x: float, y: int) -> float:
+    """Computes the product of a floating point number and an integer"""
+
     assert is_type(x, float) and is_type(y, int)
     return x * y
 
 
-# Call functions and print the data type of the resulting answer
 def main():
+    """Call functions and print the data type of the resulting answer"""
+
     inum = randint(0, 50)
     fnum = random() * randint(0, 10)
 
