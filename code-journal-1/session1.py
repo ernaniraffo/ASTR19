1d04d91d96aa1345b7f3ba00b376fb438e9e5fc5
ernaniraffo <ernani.raffo@icloud.com>
2024-01-11 12:23:27 -0800

    Formatting according to PyLint

--- code-journal-1/session1.py
+++ code-journal-1/session1.py
@@ -1,24 +1,33 @@
-# Write a Python program to print out your full name, your preferred pronouns (optional), 
-# and two sentences about your favorite movie and your favorite food.
+"""
+Write a Python program to print out your full name, your preferred pronouns (optional), 
+and two sentences about your favorite movie and your favorite food.
+"""
 
 
 def print_name():
+    """Prints my name"""
     print("Name: Ernani Raffo")
 
 
 def print_pronouns():
+    """Prints my pronouns"""
     print("Pronouns: He/Him")
 
 
 def print_favorite_movie():
-    print("My favorite movie at the moment is Interstellar because I love the science behind the events of the movie.")
+    """Prints my favorite movie"""
+    print("My favorite movie at the moment is Interstellar because I love the science behind the \
+          events of the movie.")
 
 
 def print_favorite_food():
-    print("I am Brazilian, so it's only right that my favorite food is Brazilian food, which includes rice and beans, beef strogonoff, and more.")
+    """Prints my favorite food"""
+    print("I am Brazilian, so it's only right that my favorite food is Brazilian food, which \
+          includes rice and beans, beef strogonoff, and more.")
 
 
 def main():
+    """Call all functions to print information"""
     functions = [print_name, print_pronouns, print_favorite_movie, print_favorite_food]
     for f in functions:
         f()
--- code-journal-1/session3.py
+++ code-journal-1/session3.py
@@ -1,15 +1,17 @@
-# Write a Python program that defines a function f(x) that returns x**3 + 8. 
-# In the main function of the program, call f(x) with x = 9 and print the result.  
-# Use an if statement that executes if the result is larger than 27 and prints “YAY!”.
+"""
+Write a Python program that defines a function f(x) that returns x**3 + 8. 
+In the main function of the program, call f(x) with x = 9 and print the result.  
+Use an if statement that executes if the result is larger than 27 and prints “YAY!”.
+"""
 
 
-# f(x) = x^3 + 8
 def f(x: int | float) -> int | float:
+    """f(x) = x^3 + 8"""
     return (x**3) + 8
 
 
-# Main program
 def main():
+    """Main program"""
     res = f(9)
     print(res)
     if res > 27:
