b888d04eced9ee149a259bc89f4dd09e2de60b4c
ernaniraffo <ernani.raffo@icloud.com>
2024-01-25 14:27:01 -0800

    uses actual tables

--- code-journal-1/session5.py
+++ code-journal-1/session5.py
@@ -3,14 +3,14 @@ Write a Python program that writes out a table of the function sin(x) vs. x,
 where x is tabulated between 0 and 2 with a thousand entries.
 Follow the basic Python program structure, including a main program function.
 """
-
 import numpy as np
+from tabulate import tabulate
 
 
 def main():
     values = np.linspace(0, 2 * np.pi, 1000)
-    for x, sin_x in zip(values, np.sin(values)):
-        print(f"sin({x}) = {sin_x}")
+    table_data = [(x, sin_x) for x, sin_x in zip(values, np.sin(values))]
+    print(tabulate(table_data, headers=['x', 'sin(x)'], floatfmt='.3f', tablefmt='rounded_grid'))
 
 
 if __name__ == "__main__":
